3e30cd2164b275e6202ec7bf60e1dda896427f7d
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-21 15:56:34 -0700

    Create initial Start Menu option prompts

--- CS131-BankingLogin-HonorsProject1/CS131-BankingLogin-HonorsProject1.cpp
+++ CS131-BankingLogin-HonorsProject1/CS131-BankingLogin-HonorsProject1.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Welcome to your eBank!\n";
+    cout << "Welcome to your eBank!\n\n";
+
+    // Start Menu options. Login or Create Account
+    int startMenu = 0;
+    while (startMenu != 1 && startMenu != 2)
+    {
+        cout << "\nWhat would you like to do?\n";
+        cout << "1 = Login\n2 = Create Account\n";
+        cin >> startMenu;
+        if (startMenu == 1)
+        {
+            cout << "\nFill in your login information below\n";
+        }
+        else if (startMenu == 2)
+        {
+            cout << "\nLet's create a new account for you!\n";
+        }
+    }
+    
 }
