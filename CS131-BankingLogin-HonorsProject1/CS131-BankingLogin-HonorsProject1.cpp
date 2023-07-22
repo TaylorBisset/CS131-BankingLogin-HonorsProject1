c559657c932f96f735adf62c71080424b2d591ee
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-21 22:16:51 -0700

    Move password creation logic to bottom of code to work on password function

--- CS131-BankingLogin-HonorsProject1/CS131-BankingLogin-HonorsProject1.cpp
+++ CS131-BankingLogin-HonorsProject1/CS131-BankingLogin-HonorsProject1.cpp
@@ -28,83 +28,6 @@ int main()
         cout << "\nInvalid option. Please try again.\n";
         break;
     }
-
-    // Create Account 
-    if (startMenu == 2)
-    {
-        
-
-        // Create password 
-        cout << endl;
-        cout << "Please create a password for your new account.\n";
-        cout << "Your password must be at least 8 characters long, \n";
-        cout << "and must contain at least:\n";
-        cout << "2 letters, 2 numbers,\n2 standard special characters (e.g. !@#$%^&*()),\nand no spaces.\n\n";
-        cout << "Remember to NEVER share your password with anyone.\n";
-
-        string password;
-        bool isPasswordValid = false;
-
-        while (!isPasswordValid)                        // initiate while loop to check for a valid password input 
-        {
-            cout << "\n\tEnter your password here: ";
-            getline(cin, password);                     // user inputs their desired password 
-            int alphaCount = 0;
-            int digitCount = 0;
-            int specialCount = 0;
-
-            if (password.size() > 7)
-            {
-                isPasswordValid = true;                 // set true, until proven otherwise 
-                for (char c : password)                 // range-based for loop 
-                {
-                    if (isspace(c))                     // checks if password contains a space 
-                    {
-                        isPasswordValid = false;
-                        cout << "\nInvalid password. Please do not use spaces.\n";
-                        break;
-                    }
-                    if (isalpha(c))                     // checks password for letters 
-                    {
-                        alphaCount++;
-                    }
-                    if (isdigit(c))                     // checks password for numbers 
-                    {
-                        digitCount++;
-                    }
-                    if (c == '!' || c == '@' ||(c >= '#' && c <= '&') || (c >= '(' && c <= '*')) // checks for special characters 
-                    {
-                        specialCount++;
-                    }
-                }
-            }
-            else
-            {
-                cout << "\nInvalid password. Please ensure there are at least 8 characters.\n";
-            }
-            if (alphaCount < 2)
-            {
-                cout << "\nInvalid password. You must use at least 2 letters.\n";
-                isPasswordValid = false;
-            }
-            if (digitCount < 2)
-            {
-                cout << "\nInvalid password. You must use at least 2 numbers.\n";
-                isPasswordValid = false;
-            }
-            if (specialCount < 2)
-            {
-                cout << "\nInvalid password. You must use at least 2 standard special characaters (e.g. !@#$%^&*()).\n";
-                isPasswordValid = false;
-            }
-        }
-
-        if (isPasswordValid == true)
-        {
-            cout << "\n\tValid password.\n\tYour password is: " << password << endl;
-        }
-    }
-
     return 0;
 }
 
@@ -135,7 +58,7 @@ void createAccount()
 {
     cout << "\nLet's create a new account for you!\n";
     string username = getValidUsername();
-    // get password using function
+    string password = getValidPassword();
 }
 
 string getValidUsername()
@@ -177,4 +100,9 @@ string getValidUsername()
         cout << "\n\tValid username.\n\tYour username is: " << username << endl;
     }
     return username;
+}
+
+string getValidPassword()
+{
+
 }
