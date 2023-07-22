605314012d7ad0358767c76009acdb0f2be4da90
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-21 22:18:48 -0700

    Password function started

--- CS131-BankingLogin-HonorsProject1/CS131-BankingLogin-HonorsProject1.cpp
+++ CS131-BankingLogin-HonorsProject1/CS131-BankingLogin-HonorsProject1.cpp
@@ -104,5 +104,72 @@ string getValidUsername()
 
 string getValidPassword()
 {
+    string password;
+    bool isPasswordValid = false;
 
+    cout << endl;
+    cout << "Please create a password for your new account.\n";
+    cout << "Your password must be at least 8 characters long, \n";
+    cout << "and must contain at least:\n";
+    cout << "2 letters, 2 numbers,\n2 standard special characters (e.g. !@#$%^&*()),\nand no spaces.\n\n";
+    cout << "Remember to NEVER share your password with anyone.\n";
+
+    while (!isPasswordValid)                        // initiate while loop to check for a valid password input 
+    {
+        cout << "\n\tEnter your password here: ";
+        getline(cin, password);                     // user inputs their desired password 
+        int alphaCount = 0;
+        int digitCount = 0;
+        int specialCount = 0;
+
+        if (password.size() > 7)
+        {
+            isPasswordValid = true;                 // set true, until proven otherwise 
+            for (char c : password)                 // range-based for loop 
+            {
+                if (isspace(c))                     // checks if password contains a space 
+                {
+                    isPasswordValid = false;
+                    cout << "\nInvalid password. Please do not use spaces.\n";
+                    break;
+                }
+                if (isalpha(c))                     // checks password for letters 
+                {
+                    alphaCount++;
+                }
+                if (isdigit(c))                     // checks password for numbers 
+                {
+                    digitCount++;
+                }
+                if (c == '!' || c == '@' || (c >= '#' && c <= '&') || (c >= '(' && c <= '*')) // checks for special characters 
+                {
+                    specialCount++;
+                }
+            }
+        }
+        else
+        {
+            cout << "\nInvalid password. Please ensure there are at least 8 characters.\n";
+        }
+        if (alphaCount < 2)
+        {
+            cout << "\nInvalid password. You must use at least 2 letters.\n";
+            isPasswordValid = false;
+        }
+        if (digitCount < 2)
+        {
+            cout << "\nInvalid password. You must use at least 2 numbers.\n";
+            isPasswordValid = false;
+        }
+        if (specialCount < 2)
+        {
+            cout << "\nInvalid password. You must use at least 2 standard special characaters (e.g. !@#$%^&*()).\n";
+            isPasswordValid = false;
+        }
+    }
+
+    if (isPasswordValid == true)
+    {
+        cout << "\n\tValid password.\n\tYour password is: " << password << endl;
+    }
 }
